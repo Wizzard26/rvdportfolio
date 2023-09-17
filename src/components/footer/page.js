4a82192c61bd5f526c10d3d09ebaa8a9f75a806b
Rene van Dinter <dinter@rhinos-media.de>
2023-09-17 19:05:08 +0200

    add react icons and first footer content

--- src/components/footer/page.js
+++ src/components/footer/page.js
@@ -2,6 +2,22 @@
 import Link from "next/link";
 import styled from "styled-components";
 import StyledComponentsRegistry from "@/lib/registry";
+import {
+    DiCss3,
+    DiHtml5,
+    DiJavascript,
+    DiNodejsSmall,
+    DiReact,
+    DiNpm,
+    DiPhp,
+    DiLess,
+    DiSass,
+    DiGithubBadge
+} from "react-icons/di";
+import {BiLogoDocker, BiLogoFacebook, BiLogoGithub, BiLogoInstagram, BiLogoLinkedin, BiLogoXing} from "react-icons/bi";
+import {SiContao, SiNextdotjs, SiShopware, SiSymfony, SiWordpress} from "react-icons/si";
+import {PiGitlabLogo} from "react-icons/pi";
+import {AiOutlineCopyrightCircle} from "react-icons/ai";
 
 export default function Footer() {
     const yearStart = 2013;
@@ -12,11 +28,47 @@ export default function Footer() {
             <StyledComponentsRegistry>
                 <StyledFooter>
                     <div className="content-inner">
-                        Footer
-                        <Link href="/dashboard">Go to Admin</Link>
+                        <div className="row">
+                            <div className="col-12 col-md-6 col-xl-4 columns">
+                                <h3>Webdevelopment</h3>
+                                <TecIcons>
+                                    <DiHtml5 />
+                                    <DiCss3 />
+                                    <DiJavascript />
+                                    <DiNpm />
+                                    <DiNodejsSmall />
+                                    <DiReact />
+                                    <DiPhp />
+                                    <DiLess />
+                                    <DiSass />
+                                    <BiLogoDocker />
+                                    <DiGithubBadge />
+                                    <PiGitlabLogo />
+                                </TecIcons>
+                                <h3>CMS & Frameworks</h3>
+                                <TecIcons>
+                                    <SiSymfony />
+                                    <SiNextdotjs />
+                                    <SiContao />
+                                    <SiWordpress />
+                                    <SiShopware />
+                                </TecIcons>
+                            </div>
+                            <div className="col-12 col-md-6 col-xl-4 columns">
+                                <h3>Social Media</h3>
+                                <SocialIcons>
+                                    <Link href="https://www.gambit24.de" target="_blank"><BiLogoGithub /></Link>
+                                    <Link href="https://www.gambit24.de" target="_blank"><BiLogoLinkedin /></Link>
+                                    <Link href="https://www.gambit24.de" target="_blank"><BiLogoXing /></Link>
+                                    <Link href="https://www.gambit24.de" target="_blank"><BiLogoFacebook /></Link>
+                                    <Link href="https://www.gambit24.de" target="_blank"><BiLogoInstagram /></Link>
+                                </SocialIcons>
+                            </div>
+                        </div>
+
                     </div>
                     <StyledEndLine>
-                        © Copyright {yearStart} - {yearNow} | Design, Code and Content by Rene van Dinter | all rights reserved
+                        <AiOutlineCopyrightCircle /> Copyright {yearStart} - {yearNow} | Design, Code and Content by Rene van Dinter | all rights reserved
                     </StyledEndLine>
                 </StyledFooter>
             </StyledComponentsRegistry>
@@ -32,6 +84,10 @@ const StyledFooter = styled.footer`
   bottom: 0;
   background-color: var(--primary);
   color: var(--fontlight);
+  
+  .columns {
+    padding: 15px 0 25px;
+  }
 `;
 
 const StyledEndLine = styled.div`
@@ -39,4 +95,27 @@ const StyledEndLine = styled.div`
   background-color: var(--dark);
   padding: 3px 15px;
   text-align: center;
-`
+`;
+
+const TecIcons = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  
+  svg {
+    height: 40px;
+    width: auto;
+  }
+`;
+
+const SocialIcons = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  
+  svg {
+    height: 40px;
+    width: auto;
+  }
+`;
+
